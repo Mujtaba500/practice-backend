8b9f99b1e6dd0db2c9e802e4b78fc9a8e5efe541
Mujtaba <126389386+Mujtaba500@users.noreply.github.com>
2024-06-15 22:14:29 +0500

    multer and cloudinary setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import app from "./app.js";
 import "dotenv/config";
 import connectDb from "./db/config.js";
+import upload from "./middleware/multer.js";
+import { uploadOnCloudinary } from "./utils/cloudinary.js";
 
 app.get("/", (req, res) => {
   res.json({
     message: "response 2nd",
+  });
+});
+
+// app.post("/upload", upload.single("meme"), async (req, res) => {
+//   // uploadOnCloudinary("./public/temp");
+//   try {
+//     // Upload image to Cloudinary
+//     console.log(req.file);
+//     const result = await cloudinary.uploader.upload(req.file.path, {
+//       resource_type: "auto",
+//     });
+//     console.log(result.url);
+//     // Send the Cloudinary URL in the response
+//     res.json({ imageUrl: result.secure_url });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Error uploading image to Cloudinary" });
+//   }
+// });
+
+app.post("/upload", upload.single("meme"), async (req, res) => {
+  const response = uploadOnCloudinary(req.file.path);
+  res.json({
+    url: response.secure_url,
   });
 });
 
